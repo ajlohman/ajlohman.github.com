e005a912b0c8c9219191007422808c767f93a605
Andrew Lohman <ajlohman@gmail.com>
2015-09-01 09:03:13 -0500

    committ hudl

--- hudl/js/global.js
+++ hudl/js/global.js
@@ -3,11 +3,36 @@
 $(function(){
 	$('#add-athlete').on('click', function (e) {
 	    $('#modal-add-athlete').modal('hide')
-
-	    $( "#add-to-roster-one" ).fadeIn( 200 );
-	    $( "#overlay-fadeout" ).delay( 1000 ).fadeOut( 200 );
-	    $( "#athlete-success" ).delay( 1200 ).fadeIn( 200 );
-	    $( "#athlete-success" ).delay( 2000 ).fadeOut( 400 );
-		
+	    $( "#add-to-roster-one" ).fadeIn( 300 );
+	    $( "#overlay-fadeout" ).delay( 1000 ).fadeOut( 300 );
+	    $( "#athlete-success" ).delay( 1300 ).fadeIn( 500 );
+	    $( "#athlete-success" ).delay( 3000 ).fadeOut( 500 );
 	});
 });
+
+// $(document).ready(function(){
+//     $("#add-athlete").click(function(){
+//         $('#modal-add-athlete').modal('hide')
+//         $('#add-to-roster-one' ).fadeIn( 300 );
+//     });
+// });
+
+$(document).ready(function(){
+	$('#filter-seasons').on('change', function(){
+		var $this = $(this),
+            $value = $this.val();
+		$('#default-roster').fadeOut( 600 );
+		$('#filter-roster').delay( 1000 ).fadeIn( 1000 );
+	});
+});
+
+
+// $(document).ready(function(){
+//     $('#mySelect').on('change', function(){
+//         var $this = $(this),
+//             $value = $this.val();
+        
+//         alert($value);
+//     });
+    
+// });
